ace1d8c146a642314bce8cfcdb8a3c54ce985913
Alex Miller <alex.k.miller@gmail.com>
2020-11-20 15:03:39 -0500

    Staging and live

--- test.py
+++ test.py
@@ -8,24 +8,28 @@ import statistics
 import time
 
 ITERATIONS = 10
+# PREFIX = "live"
+# MAIN_URL = "https://iatidatastore.iatistandard.org"
+PREFIX = "staging"
+MAIN_URL = "https://test-xslt.iati.cloud"
 
 def main():
     test_urls = [
-        "https://iatidatastore.iatistandard.org/search/transaction?q=((transaction_recipient_country_code:(NG)%20OR%20activity_recipient_country_code:(NG))%20AND%20transaction_date_iso_date:[2015-12-31T00:00:00Z%20TO%20*])&wt=csv&tr=transaction-csv.xsl&rows=10",
-        "https://iatidatastore.iatistandard.org/search/activity?q=recipient_country_code:(NG)&wt=xslt&tr=activity-csv.xsl&rows=10",
-        "https://iatidatastore.iatistandard.org/search/transaction?q=((transaction_recipient_country_code:(NG)%20OR%20activity_recipient_country_code:(NG))%20AND%20transaction_date_iso_date:[2015-12-31T00:00:00Z%20TO%20*])&wt=csv&tr=transaction-csv.xsl&rows=100",
-        "https://iatidatastore.iatistandard.org/search/activity?q=recipient_country_code:(NG)&wt=xslt&tr=activity-csv.xsl&rows=100",
-        "https://iatidatastore.iatistandard.org/search/transaction?q=((transaction_recipient_country_code:(NG)%20OR%20activity_recipient_country_code:(NG))%20AND%20transaction_date_iso_date:[2015-12-31T00:00:00Z%20TO%20*])&wt=csv&tr=transaction-csv.xsl&rows=1000",
-        "https://iatidatastore.iatistandard.org/search/activity?q=recipient_country_code:(NG)&wt=xslt&tr=activity-csv.xsl&rows=1000",
-        "https://iatidatastore.iatistandard.org/search/transaction?q=((transaction_recipient_country_code:(NG)%20OR%20activity_recipient_country_code:(NG))%20AND%20transaction_date_iso_date:[2015-12-31T00:00:00Z%20TO%20*])&wt=csv&tr=transaction-csv.xsl&rows=10000",
-        "https://iatidatastore.iatistandard.org/search/activity?q=recipient_country_code:(NG)&wt=xslt&tr=activity-csv.xsl&rows=10000",
-        "https://iatidatastore.iatistandard.org/search/transaction?q=((transaction_recipient_country_code:(NG)%20OR%20activity_recipient_country_code:(NG))%20AND%20transaction_date_iso_date:[2015-12-31T00:00:00Z%20TO%20*])&wt=csv&tr=transaction-csv.xsl&rows=100000",
-        "https://iatidatastore.iatistandard.org/search/activity?q=recipient_country_code:(NG)&wt=xslt&tr=activity-csv.xsl&rows=100000",
-        "https://iatidatastore.iatistandard.org/search/transaction?q=((transaction_recipient_country_code:(NG)%20OR%20activity_recipient_country_code:(NG))%20AND%20transaction_date_iso_date:[2015-12-31T00:00:00Z%20TO%20*])&wt=csv&tr=transaction-csv.xsl&rows=1000000",
-        "https://iatidatastore.iatistandard.org/search/activity?q=recipient_country_code:(NG)&wt=xslt&tr=activity-csv.xsl&rows=1000000",
+        "/search/transaction?q=((transaction_recipient_country_code:(NG)%20OR%20activity_recipient_country_code:(NG))%20AND%20transaction_date_iso_date:[2015-12-31T00:00:00Z%20TO%20*])&wt=csv&tr=transaction-csv.xsl&rows=10",
+        "/search/activity?q=recipient_country_code:(NG)&wt=xslt&tr=activity-csv.xsl&rows=10",
+        "/search/transaction?q=((transaction_recipient_country_code:(NG)%20OR%20activity_recipient_country_code:(NG))%20AND%20transaction_date_iso_date:[2015-12-31T00:00:00Z%20TO%20*])&wt=csv&tr=transaction-csv.xsl&rows=100",
+        "/search/activity?q=recipient_country_code:(NG)&wt=xslt&tr=activity-csv.xsl&rows=100",
+        "/search/transaction?q=((transaction_recipient_country_code:(NG)%20OR%20activity_recipient_country_code:(NG))%20AND%20transaction_date_iso_date:[2015-12-31T00:00:00Z%20TO%20*])&wt=csv&tr=transaction-csv.xsl&rows=1000",
+        "/search/activity?q=recipient_country_code:(NG)&wt=xslt&tr=activity-csv.xsl&rows=1000",
+        "/search/transaction?q=((transaction_recipient_country_code:(NG)%20OR%20activity_recipient_country_code:(NG))%20AND%20transaction_date_iso_date:[2015-12-31T00:00:00Z%20TO%20*])&wt=csv&tr=transaction-csv.xsl&rows=10000",
+        "/search/activity?q=recipient_country_code:(NG)&wt=xslt&tr=activity-csv.xsl&rows=10000",
+        "/search/transaction?q=((transaction_recipient_country_code:(NG)%20OR%20activity_recipient_country_code:(NG))%20AND%20transaction_date_iso_date:[2015-12-31T00:00:00Z%20TO%20*])&wt=csv&tr=transaction-csv.xsl&rows=100000",
+        "/search/activity?q=recipient_country_code:(NG)&wt=xslt&tr=activity-csv.xsl&rows=100000",
+        "/search/transaction?q=((transaction_recipient_country_code:(NG)%20OR%20activity_recipient_country_code:(NG))%20AND%20transaction_date_iso_date:[2015-12-31T00:00:00Z%20TO%20*])&wt=csv&tr=transaction-csv.xsl&rows=1000000",
+        "/search/activity?q=recipient_country_code:(NG)&wt=xslt&tr=activity-csv.xsl&rows=1000000",
     ]
     fieldnames = ['url', 'response_codes', 'response_time_avg', 'response_time_min', 'response_time_max']
-    filename = "output_{}.csv".format(datetime.now().strftime("%Y_%m_%d"))
+    filename = "{}_output_{}.csv".format(PREFIX, datetime.now().strftime("%Y_%m_%d"))
 
     with open(filename, 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -36,9 +40,15 @@ def main():
             response_codes = list()
             response_times = list()
             for _ in bar(range(0, ITERATIONS)):
-                response = requests.get(test_url)
+                start_time = datetime.now()
+                try:
+                    response = requests.get(MAIN_URL + test_url)
+                except:
+                    continue
+                end_time = datetime.now()
+                time_diff = end_time - start_time
                 response_codes.append(response.status_code)
-                response_times.append(response.elapsed.total_seconds())
+                response_times.append(time_diff.total_seconds())
                 time.sleep(1)
 
             writer.writerow(
